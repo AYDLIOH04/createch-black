edbac5076cb2a75e76469d46e56e514e67c7dd32
Pavel Biryuchev <108347976+AYDLIOH04@users.noreply.github.com>
2023-02-06 00:58:35 +0500

    Telegram

---
+++ ajax.php
@@ -0,0 +1,52 @@
+<?php
+$tg_bot_token = "6161204777:AAGQMlkmMAS8gECJAbXDNXBlIGOfJtZ2Uno";
+$chat_id = "-888721777";
+
+$text = "========= Получен запрос =========" . "\n" . "\n";
+
+foreach ($_POST as $key => $val) {
+    $text .= $key . ": " . $val . "\n" . "\n";
+}
+
+$text .= "IP - " . $_SERVER['REMOTE_ADDR'] . "\n";
+$text .= "Дата отправки - " . date('d.m.y H:i:s') . "\n";
+
+$text .= "==================================" . "\n" . "\n";
+
+
+$param = [
+    "chat_id" => $chat_id,
+    "text" => $text
+];
+
+$url = "https://api.telegram.org/bot" . $tg_bot_token . "/sendMessage?" . http_build_query($param);
+
+var_dump($text);
+
+file_get_contents($url);
+
+foreach ( $_FILES as $file ) {
+
+    $url = "https://api.telegram.org/bot" . $tg_bot_token . "/sendDocument";
+
+    move_uploaded_file($file['tmp_name'], $file['name']);
+
+    $document = new \CURLFile($file['name']);
+
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, ["chat_id" => $chat_id, "document" => $document]);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ["Content-Type:multipart/form-data"]);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
+    $out = curl_exec($ch);
+
+    curl_close($ch);
+
+    unlink($file['name']);
+}
+
+die('1');
--- js/email.js
+++ js/email.js
@@ -1,99 +1,127 @@
 'use strict'
 
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('form')
-    form.addEventListener('submit', formSend)
+const forma = document.getElementById('form')
+forma.addEventListener('submit', formSend)
 
-    async function formSend(e) {
-        e.preventDefault()
-        let error = formValidate(form)
-        let formData = new FormData(form)
-        if (error === 0){
-            const popupContent = document.querySelector('.popup__content')
-            popupContent.classList.add('_sending')
+async function formSend(e) {
+    e.preventDefault()
+    let error = formValidate(forma)
+    let formData = new FormData(forma)
+    if (error === 0){
+        const popupContent = document.querySelector('.popup__content')
+        popupContent.classList.add('_sending')
 
-            // TODO Отправка на почту тут
-            // let response = await fetch('sendmail.php', {
-            //     method: 'POST',
-            //     body: formData
-            // })
-            // 
-            // console.log(response)
-            // 
-            // if (response.ok){
-            //     let result = await response.json()
-            //     alert(result.message)
-            // } else {
-            //     alert("Ошибка!")
-            // }
-            
-            
-            
+
+        event.preventDefault();
+    
+        let form = $('.form__body'),
+            submit = $('.submit', form),
+            data = new FormData()
+    
+        
+        $('.submit', form).val('Отправка...');
+    
+        data.append( 'Название организации', 	                  $('[name="comp"]', form).val() );
+        data.append( 'Имя', 		                              $('[name="name"]', form).val() );
+        data.append( 'E-mail', 		                              $('[name="email"]', form).val() );
+        data.append( 'Телефон', 		                          $('[name="number"]', form).val() );
+        data.append( 'Необходимые технические характеристики',    $('[name="techharact"]', form).val() );
+        data.append( 'Наименование запрашиваемого оборудования',  $('[name="id"]', form).val() );
+        data.append( 'Артикул',                                   $('[name="articl"]', form).val() );
+        data.append( 'Количество',                                $('[name="count"]', form).val() );
+        data.append( 'Коментарии',                                $('[name="coments"]', form).val() );
+        
+        $.ajax({
+            url: 'ajax.php',
+            type: 'POST',
+            data: data,
+            cache: false,
+            dataType: 'json',
+            processData: false,
+            contentType: false,
+            xhr: function() {
+                let myXhr = $.ajaxSettings.xhr();
+    
+                if ( myXhr.upload ) {
+                    myXhr.upload.addEventListener( 'progress', function(e) {
+                        if ( e.lengthComputable ) {
+                            let percentage = ( e.loaded / e.total ) * 100;
+                                percentage = percentage.toFixed(0);
+                            $('.submit', form)
+                                .html( percentage + '%' );
+                        }
+                    }, false );
+                }
+    
+                return myXhr;
+            }
+        });
+        
+        setTimeout(() => {
+            popupContent.classList.remove('_sending')
+
             setTimeout(() => {
-                // TODO Когда отправка на почту будет готова, нужно сделать проверку if(response.ok)
-                const mail = createObject("._form")
-                formAlert(mail) 
-                
-                form.reset()
-                popupContent.classList.remove('_sending')
-            }, 1000)
+                const mail = createObject()
+                formAlert(mail)
+                forma.reset()
+            }, 100)
+        }, 500)
 
+    } else {
+        alert('Заполните обязательные поля!')
+    }
+}
+
+function createObject() {
+    let data = document.querySelectorAll("._form")
+    const mail = {}
+    for (let i = 0; i < data.length; i+=2) {
+        mail[data[i].textContent] = data[i + 1].value
+    }
+    return mail
+}
+
+function formAlert(mail) {
+    let ans = 'ЗАПРОС ОТПРАВЛЕН\n\n' 
+    for (var key in mail) {
+        ans += `${key}:   ${mail[key]}\n`
+    }
+    alert(ans)
+}
+
+function formValidate(form) {
+    let error = 0
+    let formReq = document.querySelectorAll('._req')
+
+    for (let index = 0; index < formReq.length; index++){
+        const input = formReq[index]
+        formRemoveError(input)
+
+        if(input.classList.contains('_email')){
+            if (emailTest(input)){
+                formAddError(input)
+                error++
+            }
         } else {
-            alert('Заполните обязательные поля!')
-        }
-    }
-
-    function createObject(className) {
-        let data = document.querySelectorAll(className)
-        const mail = {}
-        for (let i = 0; i < data.length; i+=2) {
-            mail[data[i].textContent] = data[i + 1].value
-        }
-        return mail
-    }
-
-    function formAlert(mail) {
-        let ans = 'ЗАПРОС ОТПРАВЛЕН\n\n' 
-        for (var key in mail) {
-            ans += `${key}:   ${mail[key]}\n`
-        }
-        alert(ans)
-    }
-
-    function formValidate(form) {
-        let error = 0
-        let formReq = document.querySelectorAll('._req')
-
-        for (let index = 0; index < formReq.length; index++){
-            const input = formReq[index]
-            formRemoveError(input)
-
-            if(input.classList.contains('_email')){
-                if (emailTest(input)){
-                    formAddError(input)
-                    error++
-                }
-            } else {
-                if (input.value === '') {
-                    formAddError(input)
-                    error++
-                }
+            if (input.value === '') {
+                formAddError(input)
+                error++
             }
         }
-        return error
     }
+    return error
+}
 
-    function formAddError(input){
-        input.parentElement.classList.add('_error')
-        input.classList.add('_error')
-    }
+function formAddError(input){
+    input.parentElement.classList.add('_error')
+    input.classList.add('_error')
+}
 
-    function formRemoveError(input){
-        input.parentElement.classList.remove('_error')
-        input.classList.remove('_error')
-    }
+function formRemoveError(input){
+    input.parentElement.classList.remove('_error')
+    input.classList.remove('_error')
+}
 
-    function emailTest(input){
-        return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value)
-    }
-})
+function emailTest(input){
+    return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value)
+}
